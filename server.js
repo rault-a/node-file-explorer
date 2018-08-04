5f8eeb4afafdc01566735f98d8e7a0b00419a652
Adrien Rault <adrien@rault.io>
2018-08-04 18:36:44 +0200

    update to more modern JS

--- server.js
+++ server.js
@@ -1,67 +1,67 @@
 'use strict';
 
-var express = require('express'),
-	app = express(),
-	path = require('path'),
-	fs = require('fs'),
-	basicAuth = require('basic-auth-connect');
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const { promisify } = require('util')
+const basicAuth = require('basic-auth-connect')
 
-var publicDir = path.join(__dirname, 'public'),
-	username = process.env.USERNAME,
-	password = process.env.PASSWORD;
+const readdir = promisify(fs.readdir)
+const stat = promisify(fs.stat)
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(basicAuth(username, password));
+const publicDir = path.join(__dirname, 'public')
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+if (username && password) {
+  app.use(basicAuth(username, password));
+}
+
 app.use(express.static(publicDir, {
-	maxAge: 86400000
+  maxAge: 86400000
 }));
 
-app.locals.moment = require('moment');
+app.locals.moment = require('moment')
 
-function humanFileSize(bytes, si) {
-	var thresh = si ? 1000 : 1024;
-	if (bytes < thresh) {
-		return bytes + ' o';
-	}
-	var units = si ? ['ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo'] : ['kio', 'MiB', 'Gio', 'Tio', 'Pio', 'Eio', 'Zio', 'Yio'];
-	var u = -1;
-	do {
-		bytes /= thresh;
-		++u;
-	} while (bytes >= thresh);
-	return bytes.toFixed(1) + ' ' + units[u];
+function humanFileSize(bytes) {
+  const units = ['o', 'ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo']
+  let u = 0
+  while (bytes >= 1000 ** (u + 1)) {
+    ++u;
+  }
+  return `${(bytes / 1000 ** u).toFixed(1)} ${units[u]}`
 }
 
-function list(directory, callback) {
-	var filesList = {};
-	fs.readdir(directory, function (err, files) {
-		if (err || files === undefined) {
-			callback({});
-			return;
-		}
-		files.forEach(function (file) {
-			var stats = fs.statSync(path.join(directory, file));
-			filesList[file] = {
-				isDir: stats.isDirectory(),
-				size: humanFileSize(stats.size, true),
-				creation: stats.ctime,
-				modification: stats.mtime
-			};
-		});
-		callback(filesList);
-	});
+async function list(directory) {
+  const files = await readdir(directory)
+  return Promise.all(files.map((file) => {
+    return new Promise(async (resolve, reject) => {
+      const stats = await stat(path.join(directory, file)).catch(reject)
+      if (!stats) {
+        return
+      }
+
+      resolve({
+        filename: file,
+        isDir: stats.isDirectory(),
+        size: humanFileSize(stats.size),
+        creation: stats.ctime.toISOString().replace('T', ' ').replace(/\.\d{3}Z/, '')
+      })
+    })
+  }))
 }
 
-app.get('/*', function (req, res) {
-	list(path.join(publicDir, req.params[0]), function (files) {
-		res.render('index', {
-			title: process.env.TITLE,
-			currentDir: req.params[0],
-			subDir: req.params[0] !== '',
-			files: files
-		});
-	});
+app.get('/*', async (req, res) => {
+  const files = await list(path.join(publicDir, req.params[0]))
+  res.render('index', {
+    title: process.env.TITLE,
+    currentDir: req.params[0],
+    subDir: req.params[0] !== '',
+    files: files
+  })
 });
 
 app.listen(process.env.PORT || 8002);
